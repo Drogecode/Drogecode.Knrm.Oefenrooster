2d7c2987584c1645dccf48fa48eab0752c0b52b6
taco@boerendroogers.nl <taco@boerendroogers.nl>
2022-10-15 21:09:02 +0200

    Very basic mudblazor

--- Drogecode.Knrm.Oefenrooster/Client/Program.cs
+++ Drogecode.Knrm.Oefenrooster/Client/Program.cs
@@ -2,6 +2,8 @@ using Drogecode.Knrm.Oefenrooster.Client;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Authentication;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using MudBlazor.Services;
+
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
@@ -9,6 +11,8 @@ builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddHttpClient("Drogecode.Knrm.Oefenrooster.ServerAPI", client => client.BaseAddress = new Uri(builder.HostEnvironment.BaseAddress))
     .AddHttpMessageHandler<BaseAddressAuthorizationMessageHandler>();
+
+builder.Services.AddMudServices();
 
 // Supply HttpClient instances that include access tokens when making requests to the server project
 builder.Services.AddScoped(sp => sp.GetRequiredService<IHttpClientFactory>().CreateClient("Drogecode.Knrm.Oefenrooster.ServerAPI"));
