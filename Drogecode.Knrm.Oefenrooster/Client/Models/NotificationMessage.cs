b9a91a0bd5fd8fb0a8a269638eaaa35b5f06d365
taco@boerendroogers.nl <taco@boerendroogers.nl>
2022-10-23 21:47:59 +0200

    More menu stuff

---
+++ Drogecode.Knrm.Oefenrooster/Client/Models/NotificationMessage.cs
@@ -0,0 +1,4 @@
+﻿namespace Drogecode.Knrm.Oefenrooster.Client.Models;
+
+public record NotificationAuthor(string DisplayName, string AvatarUlr);
+public record NotificationMessage(string Id, string Title, string Except, string Category, DateTime PublishDate, string ImgUrl, IEnumerable<NotificationAuthor> Authors, Type ContentComponent);
--- Drogecode.Knrm.Oefenrooster/Client/Shared/Layout/NavMenu.razor.cs
+++ Drogecode.Knrm.Oefenrooster/Client/Shared/Layout/NavMenu.razor.cs
@@ -1,20 +1,11 @@
 ﻿using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Authentication;
-using System;
-using System.Xml.Linq;
 
 namespace Drogecode.Knrm.Oefenrooster.Client.Shared.Layout;
 public sealed partial class NavMenu
 {
-    [Inject] AuthenticationStateProvider AuthenticationStateProvider { get; set; }
     [Inject] SignOutSessionStateManager SignOutManager { get; set; }
     [Inject] NavigationManager Navigation { get; set; }
-
-    private AuthenticationState? _authState;
-    protected override async Task OnParametersSetAsync()
-    {
-        _authState = await AuthenticationStateProvider.GetAuthenticationStateAsync();
-    }
 
     private async Task BeginLogout(MouseEventArgs args)
     {
