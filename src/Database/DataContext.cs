a6d39cfd48378b31072b6a6f3f0a6137eabea9dc
Taco Droogers <34547552+TDroogers@users.noreply.github.com>
2022-12-04 17:47:48 +0100

    Unready PostgreSQL

---
+++ src/Database/DataContext.cs
@@ -0,0 +1,29 @@
+﻿using Drogecode.Knrm.Oefenrooster.Database.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Drogecode.Knrm.Oefenrooster.Database
+{
+    public class DataContext : DbContext
+    {
+        private readonly IConfiguration Configuration;
+        public DbSet<Users> Users { get; set; }
+
+        public DataContext(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public override void OnConfiguring(DbContextOptionsBuilder options)
+        {
+            // connect to postgres with connection string from app settings
+            options.UseNpgsql(Configuration.GetConnectionString("postgresDB"));
+        }
+
+    }
+}
