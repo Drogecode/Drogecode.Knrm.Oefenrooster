e6101a0ba6e69862041924b91d221d259709af6f
Taco Droogers <34547552+TDroogers@users.noreply.github.com>
2022-12-19 21:23:14 +0100

    Azure Graph try 1

--- src/Server/Controllers/UserController.cs
+++ src/Server/Controllers/UserController.cs
@@ -1,7 +1,10 @@
-﻿using Drogecode.Knrm.Oefenrooster.Server.Services.Interfaces;
+﻿using Azure.Identity;
+using Drogecode.Knrm.Oefenrooster.Server.Helpers;
+using Drogecode.Knrm.Oefenrooster.Server.Services.Interfaces;
 using Drogecode.Knrm.Oefenrooster.Shared.Models;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Graph;
 using Microsoft.Identity.Web.Resource;
 using System.Security.Claims;
 
@@ -14,7 +17,6 @@ public class UserController : ControllerBase
 {
     private readonly ILogger<UserController> _logger;
     private readonly IUserService _userService;
-
     public UserController(ILogger<UserController> logger, IUserService userService)
     {
         _logger = logger;
@@ -22,13 +24,49 @@ public class UserController : ControllerBase
     }
 
     [HttpGet]
-    public DrogeUser Get()
+    public async Task<DrogeUser> Get()
     {
+        try
+        {
+            var settings = Settings.LoadSettings();
+            InitializeGraph(settings);
+            await DisplayAccessTokenAsync();
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine(ex);
+        }
         var userId = new Guid(User?.FindFirstValue("http://schemas.microsoft.com/identity/claims/objectidentifier") ?? throw new Exception("No objectidentifier found"));
         var userName = User?.FindFirstValue("name") ?? throw new Exception("No userName found");
         var userEmail = User?.FindFirstValue("preferred_username") ?? throw new Exception("No userEmail found");
         var result = _userService.GetOrSetUserFromDb(userId, userName, userEmail);
         
         return result;
+    }
+
+    private void InitializeGraph(Settings settings)
+    {
+        GraphHelper.InitializeGraphForUserAuth(settings,
+            (info, cancel) =>
+            {
+                // Display the device code message to
+                // the user. This tells them
+                // where to go to sign in and provides the
+                // code to use.
+                Console.WriteLine(info.Message);
+                return Task.FromResult(0);
+            });
+    }
+    private async Task DisplayAccessTokenAsync()
+    {
+        try
+        {
+            var userToken = await GraphHelper.GetUserTokenAsync();
+            Console.WriteLine($"User token: {userToken}");
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine($"Error getting user access token: {ex.Message}");
+        }
     }
 }
---
+++ src/Server/Helpers/GraphHelper.cs
@@ -0,0 +1,41 @@
+﻿using Azure.Core;
+using Azure.Identity;
+using Microsoft.Graph;
+
+namespace Drogecode.Knrm.Oefenrooster.Server.Helpers;
+
+public class GraphHelper
+{
+    // Settings object
+    private static Settings? _settings;
+    // User auth token credential
+    private static DeviceCodeCredential? _deviceCodeCredential;
+    // Client configured with user authentication
+    private static GraphServiceClient? _userClient;
+
+    public static void InitializeGraphForUserAuth(Settings settings,
+        Func<DeviceCodeInfo, CancellationToken, Task> deviceCodePrompt)
+    {
+        _settings = settings;
+
+        _deviceCodeCredential = new DeviceCodeCredential(deviceCodePrompt,
+            settings.TenantId, settings.ClientId);
+
+        _userClient = new GraphServiceClient(_deviceCodeCredential, settings.GraphUserScopes);
+    }
+
+    public static async Task<string> GetUserTokenAsync()
+    {
+        // Ensure credential isn't null
+        _ = _deviceCodeCredential ??
+            throw new System.NullReferenceException("Graph has not been initialized for user auth");
+
+        // Ensure scopes isn't null
+        _ = _settings?.GraphUserScopes ?? throw new System.ArgumentNullException("Argument 'scopes' cannot be null");
+
+        // Request token with given scopes
+        var context = new TokenRequestContext(_settings.GraphUserScopes);
+        var response = await _deviceCodeCredential.GetTokenAsync(context);
+        return response.Token;
+    }
+}
