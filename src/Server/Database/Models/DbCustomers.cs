e453286b9d34157ae31c67f7374f405b3b9c10b7
Taco Droogers <34547552+TDroogers@users.noreply.github.com>
2022-12-19 21:21:56 +0100

    Minor db changes

---
+++ src/Server/Database/Models/DbCustomers.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Drogecode.Knrm.Oefenrooster.Server.Database.Models;
+
+[Table("Customers")]
+public class DbCustomers
+{
+    public Guid Id { get; set; }
+    public string Name { get; set; }
+    public DateTime Created { get; set; }
+}
