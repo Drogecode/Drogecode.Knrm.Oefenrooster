14521621309035fd09cf2aaae680367c1906f774
Taco Droogers <34547552+TDroogers@users.noreply.github.com>
2023-01-09 22:45:21 +0100

    Changes not saved.

--- src/Server/Database/DataContext.cs
+++ src/Server/Database/DataContext.cs
@@ -22,12 +22,14 @@ namespace Drogecode.Knrm.Oefenrooster.Server.Database
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
-            //demo seed data
+            //Users
             modelBuilder.Entity<DbUsers>(entity => { entity.Property(e => e.Id).IsRequired(); });
             modelBuilder.Entity<DbUsers>(entity => { entity.Property(e => e.Name).IsRequired(); });
             modelBuilder.Entity<DbUsers>(entity => { entity.Property(e => e.Email).IsRequired(); });
+            //demo seed data
             modelBuilder.Entity<DbUsers>(entity => { entity.HasData(new DbUsers { Name = "from model creating", Id = new Guid("46a4ddb6-412b-4329-b48f-ed681c96bc26"), Email = "test@drogecode.nl", Created = new DateTime(1992,9,4,6,30,42,DateTimeKind.Utc) }); });
 
+            //Customers
             modelBuilder.Entity<DbCustomers>(entity => { entity.Property(e => e.Id).IsRequired(); });
             modelBuilder.Entity<DbCustomers>(entity => { entity.Property(e => e.Name).IsRequired(); });
             modelBuilder.Entity<DbCustomers>(entity => { entity.Property(e => e.Created).IsRequired(); });
--- src/Server/Database/Migrations/README.md
+++ src/Server/Database/Migrations/README.md
@@ -1,5 +1,6 @@
 # How to:
 
 1. Open: "Package Manager Console"
-2. Run: dotnet ef migrations add initial --output-dir Database/Migrations --project ./src/Server/Drogecode.Knrm.Oefenrooster.Server.csproj
-3. Run: dotnet ef database update --output-dir Database/Migrations --project ./Drogecode.Knrm.Oefenrooster.Server.csproj
+2. cd .\src\Server
+3. dotnet ef migrations add initial -o Database/Migrations --project ./Drogecode.Knrm.Oefenrooster.Server.csproj
+4. dotnet ef database update --project ./Drogecode.Knrm.Oefenrooster.Server.csproj
