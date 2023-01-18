71286d9e5a2b5ecab226d509cf48c95acaa53f53
Taco Droogers <34547552+TDroogers@users.noreply.github.com>
2023-01-18 19:27:05 +0100

    Works?

--- src/Client/Pages/Index.razor.cs
+++ src/Client/Pages/Index.razor.cs
@@ -1,18 +1,23 @@
-﻿namespace Drogecode.Knrm.Oefenrooster.Client.Pages;
+﻿using System.Security.Claims;
+
+namespace Drogecode.Knrm.Oefenrooster.Client.Pages;
 public sealed partial class Index
 {
     [Inject] AuthenticationStateProvider AuthenticationStateProvider { get; set; } = default!;
     [Inject] HttpClient Http { get; set; } = default!;
     private bool _isAuthenticated;
     private string _name = string.Empty;
+    private ClaimsPrincipal _user;
     protected override async Task OnParametersSetAsync()
     {
         var authState = await AuthenticationStateProvider.GetAuthenticationStateAsync();
+        _user = authState.User;
         _isAuthenticated = authState.User?.Identity?.IsAuthenticated ?? false;
         if (_isAuthenticated)
         {
             var dbUser = await Http.GetFromJsonAsync<DrogeUser>("User");
             _name = authState!.User!.Identity!.Name ?? string.Empty;
+            
         }
     }
 }
