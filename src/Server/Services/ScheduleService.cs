5db6ecd936187fe20de62dd858c4a02f4a8cf460
Taco Droogers <34547552+TDroogers@users.noreply.github.com>
2023-01-28 14:08:21 +0100

    For Schedule, do not show defaults.

--- src/Server/Services/ScheduleService.cs
+++ src/Server/Services/ScheduleService.cs
@@ -149,15 +149,12 @@ public class ScheduleService : IScheduleService
         var d = DateTime.UtcNow.StartOfWeek(System.DayOfWeek.Monday).AddDays(relativeWeek * 7);
         var startDate = DateOnly.FromDateTime(d);
         var tillDate = DateOnly.FromDateTime(d.AddDays(6));
-        var defaults = _database.RoosterDefaults.Where(x => x.CustomerId == customerId).ToList();
         var trainings = _database.RoosterTrainings.Where(x => x.CustomerId == customerId && x.Date >= startDate && x.Date <= tillDate).ToList();
         var availables = _database.RoosterAvailables.Where(x => x.CustomerId == customerId && x.Date >= startDate && x.Date <= tillDate).ToList();
 
         var scheduleDate = startDate;
         do
         {
-            var defaultsFound = new List<Guid?>();
-            var defaultsToday = defaults.Where(x => x.WeekDay == scheduleDate.DayOfWeek);
             var trainingsToday = trainings.Where(x => x.Date == scheduleDate).ToList();
             if (trainingsToday.Count > 0)
             {
@@ -171,20 +168,6 @@ public class ScheduleService : IScheduleService
                         Date = training.Date,
                         StartTime = training.StartTime,
                         EndTime = training.EndTime,
-                    });
-                    defaultsFound.Add(training.RoosterDefaultId);
-                }
-            }
-            foreach (var def in defaultsToday)
-            {
-                if (!defaultsFound.Contains(def.Id))
-                {
-                    result.Planners.Add(new Shared.Models.Schedule.Planner
-                    {
-                        DefaultId = def.Id,
-                        Date = scheduleDate,
-                        StartTime = scheduleDate.ToDateTime(def.StartTime, DateTimeKind.Utc),
-                        EndTime = scheduleDate.ToDateTime(def.EndTime, DateTimeKind.Utc)
                     });
                 }
             }
