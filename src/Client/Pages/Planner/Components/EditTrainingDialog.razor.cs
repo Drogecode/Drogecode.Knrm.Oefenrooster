6095a6b34b73715ae5fb3e59447f5d4aebd72e02
Taco Droogers <34547552+TDroogers@users.noreply.github.com>
2023-02-21 22:48:20 +0100

    #4 Toogle vehicle works

--- src/Client/Pages/Planner/Components/EditTrainingDialog.razor.cs
+++ src/Client/Pages/Planner/Components/EditTrainingDialog.razor.cs
@@ -12,21 +12,46 @@ public sealed partial class EditTrainingDialog : IDisposable
     [Inject] private IStringLocalizer<EditTrainingDialog> L { get; set; } = default!;
     [Inject] private IStringLocalizer<App> LApp { get; set; } = default!;
     [Inject] private ScheduleRepository _scheduleRepository { get; set; } = default!;
+    [Inject] private VehicleRepository _vehicleRepository { get; set; } = default!;
     [CascadingParameter] MudDialogInstance MudDialog { get; set; } = default!;
     [Parameter] public PlannedTraining Planner { get; set; } = default!;
     [Parameter] public RefreshModel Refresh { get; set; } = default!;
     [Parameter] public List<DrogeVehicle>? Vehicles { get; set; }
     private CancellationTokenSource _cls = new();
+    private List<DrogeLinkVehicleTraining>? _linkVehicleTraining;
+
+    protected override async Task OnParametersSetAsync()
+    {
+        if (Planner.TrainingId != null)
+            _linkVehicleTraining = await _vehicleRepository.GetForTrainingAsync(Planner.TrainingId ?? throw new ArgumentNullException("Planner.TrainingId"));
+    }
 
     void Cancel() => MudDialog.Cancel();
-    private async Task Submit() {
+    private async Task Submit()
+    {
 
-        MudDialog.Close(DialogResult.Ok(true)); 
+        MudDialog.Close(DialogResult.Ok(true));
     }
 
     private async Task CheckChanged(bool toggled, DrogeVehicle vehicle)
     {
-
+        var d = _linkVehicleTraining?.FirstOrDefault(x => x.Vehicle == vehicle.Id);
+        if (d != null)
+        {
+            Console.WriteLine("d is not null");
+            d.IsSelected = toggled;
+        }
+        else
+        {
+            Console.WriteLine("d is null");
+            _linkVehicleTraining?.Add(new DrogeLinkVehicleTraining
+            {
+                IsSelected = toggled,
+                Vehicle = vehicle.Id,
+                RoosterTrainingId = Planner.TrainingId ?? throw new ArgumentNullException("Planner.TrainingId")
+            });
+        }
+        StateHasChanged();
     }
 
     public void Dispose()
