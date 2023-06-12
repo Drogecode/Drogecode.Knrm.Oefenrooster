2c86f213f5ca5a1a9fa086b02e768c9ed2ca92ff
Taco Droogers <34547552+TDroogers@users.noreply.github.com>
2023-06-12 12:26:37 +0200

    Table FixedHeader

--- src/Client/Repositories/CalendarItemRepository.cs
+++ src/Client/Repositories/CalendarItemRepository.cs
@@ -1,4 +1,5 @@
-﻿using Drogecode.Knrm.Oefenrooster.ClientGenerator.Client;
+﻿using Drogecode.Knrm.Oefenrooster.Client.Services.Interfaces;
+using Drogecode.Knrm.Oefenrooster.ClientGenerator.Client;
 using Drogecode.Knrm.Oefenrooster.Shared.Models.CalendarItem;
 
 namespace Drogecode.Knrm.Oefenrooster.Client.Repositories;
@@ -6,22 +7,30 @@ namespace Drogecode.Knrm.Oefenrooster.Client.Repositories;
 public class CalendarItemRepository
 {
     private readonly ICalendarItemClient _calendarItemClient;
+    private readonly IOfflineService _offlineService;
 
-    public CalendarItemRepository(ICalendarItemClient calendarItemClient)
+    private const string MONTHITEMS = "Mon_it_{0}_{1}";
+    private const string DAYITEMS = "Day_it_{0}_{1}";
+
+    public CalendarItemRepository(ICalendarItemClient calendarItemClient, IOfflineService offlineService)
     {
         _calendarItemClient = calendarItemClient;
+        _offlineService = offlineService;
     }
 
     public async Task<GetMonthItemResponse?> GetMonthItemAsync(int year, int month, CancellationToken clt)
     {
-        var response = await _calendarItemClient.GetMonthItemsAsync(year, month, clt);
+        var response = await _offlineService.CachedRequestAsync(string.Format(MONTHITEMS, year, month),
+            async () => await _calendarItemClient.GetMonthItemsAsync(year, month, clt),
+            clt: clt);
         return response;
-
     }
 
     public async Task<GetDayItemResponse?> GetDayItemsAsync(DateRange dateRange, CancellationToken clt)
     {
-        var response = await _calendarItemClient.GetDayItemsAsync(dateRange.Start!.Value.Year, dateRange.Start!.Value.Month, dateRange.Start!.Value.Day, dateRange.End!.Value.Year, dateRange.End!.Value.Month, dateRange.End!.Value.Day, clt);
+        var response = await _offlineService.CachedRequestAsync(string.Format(DAYITEMS, dateRange.Start?.ToString("yyMMdd"), dateRange.End?.ToString("yyMMdd")),
+            async () => await _calendarItemClient.GetDayItemsAsync(dateRange.Start!.Value.Year, dateRange.Start!.Value.Month, dateRange.Start!.Value.Day, dateRange.End!.Value.Year, dateRange.End!.Value.Month, dateRange.End!.Value.Day, clt),
+            clt: clt);
         return response;
     }
 }
