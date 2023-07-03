ef4995635949f668f13ee6ec2333c25968cb7a62
Taco Droogers <34547552+TDroogers@users.noreply.github.com>
2023-07-03 21:39:56 +0200

    PreCom WIP

--- src/PreCom/Program.cs
+++ src/PreCom/Program.cs
@@ -1,15 +1,16 @@
 ﻿using Drogecode.Knrm.Oefenrooster.PreCom;
 using Drogecode.Knrm.Oefenrooster.PreCom.Models;
+using Microsoft.Extensions.Configuration;
 using System.Net.Http.Headers;
 
+var configuration = new ConfigurationBuilder().AddJsonFile($"appsettings.json").AddEnvironmentVariables().Build();
 using HttpClient client = new();
 client.DefaultRequestHeaders.Accept.Clear();
-client.DefaultRequestHeaders.Accept.Add(
-    new MediaTypeWithQualityHeaderValue("application/vnd.github.v3+json"));
+client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/vnd.github.v3+json"));
 client.DefaultRequestHeaders.Add("User-Agent", ".NET Foundation Repository Reporter");
 
 var preComClient = new PreComClient(client, "drogecode");
-await preComClient.Login("1267517Droogers", "WhvdI6bx8vAYGxm6YQ4g");
+await preComClient.Login(configuration.GetRequiredSection("PreCom")["User"] ?? "", configuration.GetRequiredSection("PreCom")["Passwoord"] ?? "");
 var schedulerAppointments = await preComClient.GetUserSchedulerAppointments(DateTime.Today, DateTime.Today.AddDays(7));
 var userGroups = await preComClient.GetAllUserGroups();
 var d = await preComClient.GetAllFunctions(userGroups[0].GroupID, DateTime.Today);
