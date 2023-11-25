a26ba04252a6d383c0898d2a72d4260e0c2a6d87
Taco Droogers <34547552+TDroogers@users.noreply.github.com>
2023-11-25 22:22:57 +0100

    WIP #11 Select user

--- src/Client/Pages/Configuration/DayItem.razor.cs
+++ src/Client/Pages/Configuration/DayItem.razor.cs
@@ -1,8 +1,13 @@
 ﻿using Drogecode.Knrm.Oefenrooster.Client.Models;
 using Drogecode.Knrm.Oefenrooster.Client.Pages.Configuration.Components;
+using Drogecode.Knrm.Oefenrooster.Client.Pages.Planner;
+using Drogecode.Knrm.Oefenrooster.Client.Repositories;
 using Drogecode.Knrm.Oefenrooster.ClientGenerator.Client;
 using Drogecode.Knrm.Oefenrooster.Shared.Models.CalendarItem;
+using Drogecode.Knrm.Oefenrooster.Shared.Models.Function;
+using Drogecode.Knrm.Oefenrooster.Shared.Models.User;
 using Microsoft.Extensions.Localization;
+using static System.Runtime.InteropServices.JavaScript.JSType;
 
 namespace Drogecode.Knrm.Oefenrooster.Client.Pages.Configuration;
 
@@ -12,13 +17,19 @@ public sealed partial class DayItem : IDisposable
     [Inject] private IStringLocalizer<App> LApp { get; set; } = default!;
     [Inject] private ICalendarItemClient _calendarItemClient { get; set; } = default!;
     [Inject] private IDialogService _dialogProvider { get; set; } = default!;
+    [Inject] private UserRepository _userRepository { get; set; } = default!;
+    [Inject] private FunctionRepository _functionRepository { get; set; } = default!;
     private CancellationTokenSource _cls = new();
     private RefreshModel _refreshModel = new();
     private List<RoosterItemDay>? _items;
+    private List<DrogeUser>? _users;
+    private List<DrogeFunction>? _functions;
 
     protected override async Task OnParametersSetAsync()
     {
         _items =  (await _calendarItemClient.GetAllFutureDayItemsAsync(30, 0))?.DayItems;
+        _users = await _userRepository.GetAllUsersAsync(false);
+        _functions = await _functionRepository.GetAllFunctionsAsync();
         _refreshModel.RefreshRequestedAsync += RefreshMeAsync;
     }
 
@@ -28,6 +39,8 @@ public sealed partial class DayItem : IDisposable
         var parameters = new DialogParameters<DayItemDialog> {
             { x=> x.DayItem, dayItem},
             { x=> x.IsNew, isNew},
+            { x=> x.Users, _users },
+            { x=> x.Functions, _functions },
             { x=> x.Refresh, _refreshModel },
         };
         DialogOptions options = new DialogOptions() { MaxWidth = MaxWidth.Medium, FullWidth = true };
