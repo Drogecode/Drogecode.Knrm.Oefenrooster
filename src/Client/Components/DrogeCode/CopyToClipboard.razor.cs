2faf759868ff0898d9355e57c06ffb8fb5b7290f
Taco Droogers <34547552+TDroogers@users.noreply.github.com>
2024-05-16 16:38:50 +0200

    Layout Copy to clipboard

--- src/Client/Components/DrogeCode/CopyToClipboard.razor.cs
+++ src/Client/Components/DrogeCode/CopyToClipboard.razor.cs
@@ -1,5 +1,4 @@
-﻿using Drogecode.Knrm.Oefenrooster.Client.Pages.PreCom;
-using Microsoft.Extensions.Localization;
+﻿using Microsoft.Extensions.Localization;
 using Microsoft.JSInterop;
 
 namespace Drogecode.Knrm.Oefenrooster.Client.Components.DrogeCode;
@@ -7,11 +6,12 @@ namespace Drogecode.Knrm.Oefenrooster.Client.Components.DrogeCode;
 public sealed partial class CopyToClipboard
 {
     // https://chrissainty.com/copy-to-clipboard-in-blazor/
+    [Inject] IStringLocalizer<CopyToClipboard> L { get; set; }
     [Inject] private IJSRuntime JSRuntime { get; set; } = default!;
     [Parameter] public string Text { get; set; }
 
     private async Task CopyTextToClipboard()
     {
-        await JSRuntime.InvokeVoidAsync("clipboardCopy.copyText", Text);
+        await JSRuntime.InvokeVoidAsync("clipboardCopy.copyText", Text, L["Copied to clipboard"].ToString());
     }
 }
