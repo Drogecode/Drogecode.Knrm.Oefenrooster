c0cb6a1dfbc08ba1479a8b3ede879369a60a64eb
Taco Droogers <34547552+TDroogers@users.noreply.github.com>
2024-09-29 17:26:43 +0200

    Fix tests

--- tests/Drogecode.Knrm.Oefenrooster.TestClient/Tests/Components/DrogeCode/MonthSelectorTests.cs
+++ tests/Drogecode.Knrm.Oefenrooster.TestClient/Tests/Components/DrogeCode/MonthSelectorTests.cs
@@ -1,21 +1,31 @@
-﻿using Drogecode.Knrm.Oefenrooster.Client.Components.DrogeCode;
+﻿using Drogecode.Knrm.Oefenrooster.Client;
+using Drogecode.Knrm.Oefenrooster.Client.Components.DrogeCode;
+using Drogecode.Knrm.Oefenrooster.TestClient.Attributes;
 
 namespace Drogecode.Knrm.Oefenrooster.TestClient.Tests.Components.DrogeCode;
 
 public class MonthSelectorTests : BlazorTestBase
 {
-    [Fact]
-    public void MonthThisYearTest()
+    [Theory]
+    [AutoFakeItEasyData]
+    public void MonthThisYearTest([Frozen] IStringLocalizer<App> L)
     {
+        Services.AddSingleton(L);
+        A.CallTo(() => L["Today"]).Returns(new LocalizedString("Today", "Today"));
+        
         var month = DateTime.Today;
         var cut = RenderComponent<MonthSelector>(parameter => parameter.Add(p => p.Month, month));
         cut.Markup.Should().Contain(month.ToString("MMMM"));
         cut.Markup.Should().NotContain(month.Year.ToString());
     }
 
-    [Fact]
-    public void MonthNotThisYearTest()
+    [Theory]
+    [AutoFakeItEasyData]
+    public void MonthNotThisYearTest([Frozen] IStringLocalizer<App> L)
     {
+        Services.AddSingleton(L);
+        A.CallTo(() => L["Today"]).Returns(new LocalizedString("Today", "Today"));
+        
         var month = new DateTime(2022, 11, 5);
         var cut = RenderComponent<MonthSelector>(parameter => parameter.Add(p => p.Month, month));
         cut.Markup.Should().Contain(month.ToString("MMMM"));
