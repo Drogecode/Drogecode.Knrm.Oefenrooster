a4e49ac98e02886885430216562c2a5a094544b1
Taco Droogers <34547552+TDroogers@users.noreply.github.com>
2024-10-12 13:10:52 +0200

    💄Minor palette change

--- src/Client/Models/Palettes/KnrmPaletteLight.cs
+++ src/Client/Models/Palettes/KnrmPaletteLight.cs
@@ -4,6 +4,6 @@ namespace Drogecode.Knrm.Oefenrooster.Client.Models.Palettes;
 
 public class KnrmPaletteLight : PaletteLight
 {
-    public override MudColor Primary { get; set; } = "#00476d";
+    public override MudColor Primary { get; set; } = "#ed6d3a";
     public override MudColor AppbarBackground { get; set; } = "#ed6d3a";
 }
--- src/Client/Shared/Layout/LandingLayout.razor.cs
+++ src/Client/Shared/Layout/LandingLayout.razor.cs
@@ -1,4 +1,5 @@
 ﻿using Drogecode.Knrm.Oefenrooster.Client.Models;
+using Drogecode.Knrm.Oefenrooster.Client.Models.Palettes;
 
 namespace Drogecode.Knrm.Oefenrooster.Client.Shared.Layout;
 
@@ -7,4 +8,10 @@ public sealed partial class LandingLayout
     private DrogeCodeGlobal _global { get; set; } = new();
     private MudThemeProvider _mudThemeProvider = new();
     private bool _isDarkMode;
+
+    private MudTheme _myCustomTheme = new()
+    {
+        PaletteLight = new KnrmPaletteLight(),
+        PaletteDark = new KnrmPaletteDark(),
+    };
 }
