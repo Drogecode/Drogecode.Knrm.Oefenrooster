c9dc25c00e14baf44bca796a7f025fa8bfb82d1b
Taco Droogers <34547552+TDroogers@users.noreply.github.com>
2025-07-06 17:53:54 +0200

    Run playwright tests from GitHub actions

--- tests/Drogecode.Knrm.Oefenrooster.Playwright/BaseTest.cs
+++ tests/Drogecode.Knrm.Oefenrooster.Playwright/BaseTest.cs
@@ -3,15 +3,15 @@ using Microsoft.Extensions.Configuration;
 
 namespace Drogecode.Knrm.Oefenrooster.Playwright;
 
-public class BaseTest : PageTest
+public abstract class BaseTest : PageTest
 {
     private readonly IConfigurationRoot _appSettings;
     protected readonly string BaseUrl;
-    
+
     protected string UserName;
     protected string UserPassword;
-    
-    public BaseTest()
+
+    protected BaseTest()
     {
         var config = new ConfigurationBuilder()
             .AddJsonFile("appSettings.json", optional: false)
@@ -19,9 +19,13 @@ public class BaseTest : PageTest
             .Build();
 
         _appSettings = config;
-        BaseUrl = config.GetValue<string>("BaseUrl")!;
+        BaseUrl = ReadSetting("Playwright:BaseUrl")!;
+        if (!BaseUrl.EndsWith('/'))
+        {
+            BaseUrl += '/';
+        }
     }
-    
+
     [SetUp]
     public virtual async Task SetUp()
     {
@@ -30,12 +34,24 @@ public class BaseTest : PageTest
 
     protected async Task BaseSetUp(bool login)
     {
-        UserName = _appSettings.GetValue<string>("Users:Basic:Name")!;
-        UserPassword = _appSettings.GetValue<string>("Users:Basic:Password")!;
+        UserName = ReadSetting("Playwright:Users:Basic:Name");
+        UserPassword = ReadSetting("Playwright:Users:Basic:Password");
         if (login)
         {
             var loginPage = new LoginPage(Page, BaseUrl);
             await loginPage.Login(UserName, UserPassword);
         }
+    }
+
+    protected string ReadSetting(string variableName)
+    {
+        var setting = Environment.GetEnvironmentVariable(variableName);
+        if (!string.IsNullOrEmpty(setting))
+            return setting;
+        setting = _appSettings.GetValue<string>(variableName);
+        if (!string.IsNullOrEmpty(setting))
+            return setting;
+        Assert.Fail($"Setting {variableName} not found");
+        throw new Exception($"Setting {variableName} not found");
     }
 }
