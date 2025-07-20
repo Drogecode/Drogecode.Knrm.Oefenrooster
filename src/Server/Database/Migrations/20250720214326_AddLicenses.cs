726ca4697e73a315daa0f2b685700223505905c4
Taco Droogers <34547552+TDroogers@users.noreply.github.com>
2025-07-20 23:51:53 +0200

    Missing Migration

---
+++ src/Server/Database/Migrations/20250720214326_AddLicenses.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Drogecode.Knrm.Oefenrooster.Server.Database.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddLicenses : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Licenses",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uuid", nullable: false),
+                    CustomerId = table.Column<Guid>(type: "uuid", nullable: false),
+                    License = table.Column<int>(type: "integer", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Licenses", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Licenses_Customers_CustomerId",
+                        column: x => x.CustomerId,
+                        principalTable: "Customers",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.InsertData(
+                table: "Licenses",
+                columns: new[] { "Id", "CustomerId", "License" },
+                values: new object[] { new Guid("f9b05a44-e0bb-42a5-9660-085d82337a60"), new Guid("d9754755-b054-4a9c-a77f-da42a4009365"), 1 });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Licenses_CustomerId",
+                table: "Licenses",
+                column: "CustomerId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Licenses");
+        }
+    }
+}
