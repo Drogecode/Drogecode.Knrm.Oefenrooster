feaa43030420e7d0b725d889a8540649b997a8dc
Taco Droogers <34547552+TDroogers@users.noreply.github.com>
2025-09-06 12:06:03 +0200

    WIP

---
+++ src/Client/Enums/RatingPeriod.cs
@@ -0,0 +1,12 @@
+﻿namespace Drogecode.Knrm.Oefenrooster.Client.Enums;
+
+public enum RatingPeriod
+{
+    SelectValue = 0,
+    OneMonth = 1,
+    OneYear = 2,
+    TwoYear = 3,
+    ThreeYear = 4,
+    FiveYear = 5,
+    All = 100,
+}
