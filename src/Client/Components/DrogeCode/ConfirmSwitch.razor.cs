8d7d448f8beb4fd283e0fdffc12b4e8ba3c25ad0
Taco Droogers <34547552+TDroogers@users.noreply.github.com>
2025-10-12 20:21:43 +0200

    Introduce ConfirmSwitch

---
+++ src/Client/Components/DrogeCode/ConfirmSwitch.razor.cs
@@ -0,0 +1,37 @@
+﻿using System.Diagnostics.CodeAnalysis;
+using Drogecode.Knrm.Oefenrooster.Client.Components.DrogeCode.Dialogs;
+
+namespace Drogecode.Knrm.Oefenrooster.Client.Components.DrogeCode;
+
+public partial class ConfirmSwitch : ComponentBase
+{
+    [Inject, NotNull] private IStringLocalizer<ConfirmSwitch>? L { get; set; }
+    [Inject, NotNull] private IDialogService? DialogProvider { get; set; }
+    [Parameter] public bool Value { get; set; }
+    [Parameter] public EventCallback<bool> ValueChanged { get; set; }
+    [Parameter] public string? Label { get; set; }
+    [Parameter] public Color Color { get; set; } = Color.Default;
+    [Parameter] public bool Disabled { get; set; }
+
+    private async Task OnValueChanged(bool value)
+    {
+        var parameters = new DialogParameters<ConfirmDialog>
+        {
+            { x => x.Value, value },
+            { x => x.Body, L["New value {0}", value] },
+        };
+        var options = new DialogOptions
+        {
+            MaxWidth = MaxWidth.Medium,
+            CloseButton = true
+        };
+        var dialog = await DialogProvider.ShowAsync<ConfirmDialog>(L["Confirm"], parameters, options);
+        var result = await dialog.Result;
+        if (result is { Canceled: false, Data: bool response })
+        {
+            Value = response;
+            await ValueChanged.InvokeAsync(response);
+            StateHasChanged();
+        }
+    }
+}
---
+++ src/Client/Components/DrogeCode/Dialogs/ConfirmDialog.razor.cs
@@ -0,0 +1,18 @@
+﻿using System.Diagnostics.CodeAnalysis;
+
+namespace Drogecode.Knrm.Oefenrooster.Client.Components.DrogeCode.Dialogs;
+
+public partial class ConfirmDialog : ComponentBase
+{
+    [Inject, NotNull] private IStringLocalizer<App>? LApp { get; set; }
+    [CascadingParameter] IMudDialogInstance MudDialog { get; set; } = default!;
+    [Parameter] public bool Value { get; set; }
+    [Parameter] public string? Body { get; set; }
+    
+    void Cancel() => MudDialog.Cancel();
+
+    private async Task Submit()
+    {
+        MudDialog.Close(DialogResult.Ok(Value));
+    }
+}
